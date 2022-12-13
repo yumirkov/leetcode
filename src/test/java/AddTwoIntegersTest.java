9fa577e5a8eb42c37af67fb1fc37b15ab0c0e1d5
yumirkov <yumirkov@gmail.com>
2022-12-13 11:46:47 +0200

    2235. Add Two Integers

---
+++ src/test/java/AddTwoIntegersTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddTwoIntegersTest {
+
+    AddTwoIntegers solution = new AddTwoIntegers();
+
+    @Test
+    void example1() {
+        // given
+        var num1 = 12;
+        var num2 = 5;
+        var expected = 17;
+        // when
+        int actual = solution.sum(num1, num2);
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void example2() {
+        // given
+        var num1 = -10;
+        var num2 = 4;
+        var expected = -6;
+        // when
+        int actual = solution.sum(num1, num2);
+        // then
+        assertEquals(expected, actual);
+    }
+}
